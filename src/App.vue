2af13c96dc50452088c90cb62d1b57335ace7f7f
v-bianjunping <v-bianjunping@360.cn>
2023-09-24 17:06:19 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <FlowTool />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FlowTool from './components/FlowTool'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    FlowTool
   }
 }
 </script>
@@ -23,6 +22,7 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  width: 100vw;
+  height: 100vh;
 }
 </style>
